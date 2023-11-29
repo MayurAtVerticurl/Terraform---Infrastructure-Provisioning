9985c65c36c3553b13af4d7437a337e57138df14
MayurAtVerticurl <144439561+MayurAtVerticurl@users.noreply.github.com>
2023-11-29 11:09:24 +0530

    fix

---
+++ README.md
@@ -0,0 +1,3 @@
+# Terraform---Infrastructure-Provisioning
+
+The Repo that has resources for creating EC2 and Network Stackusing TERRAFORM
--- main.tf
+++ main.tf
@@ -92,11 +92,6 @@ resource "aws_subnet" "publicsubnet2c" {
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
 
-    route {
-        cidr_block = "10.0.1.0/24"
-        gateway_id = aws_internet_gateway.gw.id
-    }
-
   tags = {
     Name = "mayur-public_route_table"
   }
